250908b354176d4438300d676ee4623e1ac6874b
Gary Ravago <ravagog@gmail.com>
2017-07-29 13:27:04 -0700

    Show gravatar image in user profile page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def gravatar_url(email, size)
+    gravatar_id = Digest::MD5::hexdigest(email).downcase
+    default_url = "https://nomster-gary-ravago.herokuapp.com/assets/user-ee0b60482c16064c4fcdcb79d9ececfec7cb764e6c1234dd02eaba36963cea24.png"
+    url = "http://gravatar.com/avatar/#{gravatar_id}.png?s=#{size}&d=#{CGI::escape(default_url)}"
+  end
 end
